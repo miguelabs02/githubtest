bbfb97ff46fe5c831201afc26c2e5f73743141dc
miguelabs02 <miguel_alejob@hotmail.com>
2019-07-30 10:42:59 -0500

    Toast

--- app/src/main/java/com/example/pruebagithub/Main2Activity.java
+++ app/src/main/java/com/example/pruebagithub/Main2Activity.java
@@ -2,6 +2,7 @@ package com.example.pruebagithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Toast.makeText(this, "Hola", Toast.LENGTH_SHORT).show();
     }
 }
